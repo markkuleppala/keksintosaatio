5497b27fe43d03d4a81c7fb0606d2cbc0b885ab8
Tommi Nikkanen <tommi.nikkanen@saunalahti.fi>
2022-11-05 20:51:38 +0200

    Add routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Main } from "./main/main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route path="experts" element={<Experts />}>
+            <Route path=":expertId" element={<Expert />} />
+            <Route path="create" element={<CreateExpert />} />
+          </Route>
+          <Route path="innovations" element={<Innovations />}>
+            <Route path=":innovationId" element={<Innovation />} />
+            <Route path="create" element={<CreateInnovation />} />
+          </Route>
+          <Route path="matcher" element={<Matcher />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+const Experts = () => {
+  return <div></div>;
+};
+
+const Expert = () => {
+  return <div></div>;
+};
+
+const CreateExpert = () => {
+  return <div></div>;
+};
+
+const Innovations = () => {
+  return <div></div>;
+};
+
+const Innovation = () => {
+  return <div></div>;
+};
+
+const CreateInnovation = () => {
+  return <div></div>;
+};
+
+const Matcher = () => {
+  return <div></div>;
+};
 
 export default App;
